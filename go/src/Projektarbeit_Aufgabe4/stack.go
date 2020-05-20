c519a437c22f616806922204414558c66de9f8ad
GuymanDudeson <p.spiegler@gmx.net>
2020-05-20 19:44:30 +0200

    Implemented Vm as specified in Source File.
    Created helper struct Stack

---
+++ go/src/Projektarbeit_Aufgabe4/stack.go
@@ -0,0 +1,25 @@
+package Projektarbeit_Aufgabe4
+
+type Stack struct {
+	slice []int
+}
+
+func(stack Stack) clear(){
+	stack.slice = stack.slice[:0]
+}
+
+func(stack Stack) isEmpty() bool {
+	return len(stack.slice) == 0
+}
+
+func(stack Stack) getTop() int{
+	return stack.slice[len(stack.slice) - 1]
+}
+
+func(stack Stack) push(val int) {
+	stack.slice = append(stack.slice, val)
+}
+
+func(stack Stack) pop() {
+	stack.slice = stack.slice[:len(stack.slice)-1]
+}
--- go/src/Projektarbeit_Aufgabe4/vm.go
+++ go/src/Projektarbeit_Aufgabe4/vm.go
@@ -1,1 +1,82 @@
 package Projektarbeit_Aufgabe4
+
+type OpCode int
+
+const (
+	VM_PUSH OpCode = 0
+	VM_PLUS OpCode = 1
+	VM_MULT OpCode = 2
+)
+
+type Code struct {
+	kind OpCode
+	val int
+}
+
+func newPush(o OpCode, i int) Code{
+	return Code{
+		kind: VM_PUSH,
+		val:  i,
+	}
+}
+
+func newPlus(o OpCode) Code{
+	return Code{
+		kind: VM_PLUS,
+		val:  0,
+	}
+}
+
+func newMult(o OpCode) Code{
+	return Code{
+		kind: VM_MULT,
+		val:  0,
+	}
+}
+
+type VM struct {
+	code  []Code
+	stack Stack
+}
+
+func newVM(c []Code) VM{
+	return VM{
+		code:  c,
+		stack: nil,
+	}
+}
+
+func(vm VM) clearStack() {
+	vm.stack.clear()
+}
+
+func(vm VM) run() int{
+	vm.clearStack()
+
+	for	_, c := range vm.code{
+		switch c.kind {
+		case VM_PUSH:
+			vm.stack.push(c.val)
+			break
+		case VM_MULT:
+			var right = vm.stack.getTop()
+			vm.stack.pop()
+			var left = vm.stack.getTop()
+			vm.stack.pop()
+			vm.stack.push(left * right)
+			break
+		case VM_PLUS:
+			var right = vm.stack.getTop()
+			vm.stack.pop()
+			var left = vm.stack.getTop()
+			vm.stack.pop()
+			vm.stack.push(left + right)
+			break
+		}
+	}
+
+	if vm.stack.isEmpty() {
+		return 0
+	}
+	return vm.stack.getTop()
+}
