f8a615fb81792973b5c15e271bf24ff2982d04d1
GuymanDudeson <p.spiegler@gmx.net>
2020-05-20 16:45:59 +0200

    Utility implemented as specified in SourceFile

--- go/src/Projektarbeit_Aufgabe4/utility.go
+++ go/src/Projektarbeit_Aufgabe4/utility.go
@@ -1,1 +1,43 @@
 package Projektarbeit_Aufgabe4
+
+type Optional struct {
+	b bool
+	val string
+}
+
+func newEmptyOptional() *Optional {
+	return &Optional{
+		b:   false,
+		val: "",
+	}
+}
+
+func newOptional(val string) *Optional{
+	return &Optional{
+		b:   true,
+		val: val,
+	}
+}
+
+func isJust(o *Optional) bool{
+	return o.b
+}
+
+func isNothing(o *Optional) bool{
+	return !o.b
+}
+
+func fromJust(o *Optional) string{
+	return o.val
+}
+
+func nothing() *Optional{
+	return newEmptyOptional()
+}
+
+func just(val string) *Optional{
+	return newOptional(val)
+}
+
+
+
